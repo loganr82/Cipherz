7a39f91dcfc7529883073b8c4b76804c0f346be4
Logan Rogers <mad@dbcs-g.k12k.com>
2020-11-20 12:07:34 -0500

    Add flatlist

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,12 +5,11 @@ import {
   View,
   SafeAreaView,
   StyleSheet,
-  TouchableHighlight,
-  Image,
+  TouchableHighlight
 } from "react-native";
 import {
   useFonts,
-  NovaSquare_400Regular,
+  NovaSquare_400Regular
 } from "@expo-google-fonts/nova-square";
 import { AppLoading } from "expo";
 import { DecipherSvg, LearnSvg, DocSvg } from "../svgs/Svgs";
@@ -18,7 +17,7 @@ import Slider from "@react-native-community/slider";
 
 export default HomeScreen = ({ navigation }) => {
   const [fontsLoaded] = useFonts({
-    NovaSquare_400Regular,
+    NovaSquare_400Regular
   });
   if (!fontsLoaded) {
     return <AppLoading />;
@@ -79,7 +78,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "space-between",
-    backgroundColor: "#39465A",
+    backgroundColor: "#39465A"
   },
   titleText: {
     fontFamily: "NovaSquare_400Regular",
@@ -87,13 +86,13 @@ const styles = StyleSheet.create({
     fontSize: 48,
     textAlign: "center",
     marginTop: 20,
-    marginBottom: 10,
+    marginBottom: 10
   },
   subTitleText: {
     fontFamily: "NovaSquare_400Regular",
     color: "#D7F8FF",
     fontSize: 18,
-    textAlign: "center",
+    textAlign: "center"
   },
   largeButton: {
     width: 200,
@@ -102,7 +101,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#CEF6FF",
     padding: 12,
     borderRadius: 100,
-    marginBottom: 20,
+    marginBottom: 20
   },
   largeButtonInner: {
     width: 175,
@@ -110,11 +109,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "white",
     borderRadius: 100,
-    padding: 20,
+    padding: 20
   },
   largeButtonText: {
     fontFamily: "NovaSquare_400Regular",
-    fontSize: 32,
+    fontSize: 32
   },
   smallButton: {
     width: 150,
@@ -122,7 +121,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#CEF6FF",
     padding: 12,
-    borderRadius: 100,
+    borderRadius: 100
   },
   smallButtonInner: {
     width: 125,
@@ -130,11 +129,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "white",
     borderRadius: 100,
-    padding: 20,
+    padding: 20
   },
   smallButtonText: {
     fontFamily: "NovaSquare_400Regular",
-    fontSize: 24,
+    fontSize: 24
   },
   leftLine: {
     // position: "absolute",
@@ -142,7 +141,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     height: 200,
     width: 14,
-    backgroundColor: "white",
+    backgroundColor: "white"
     // top: 300,
     // left: 110
   },
@@ -152,8 +151,8 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     height: 200,
     width: 14,
-    backgroundColor: "white",
+    backgroundColor: "white"
     // left: 250,
     // top: 300
-  },
+  }
 });
--- screens/IdentifyNumbers.js
+++ screens/IdentifyNumbers.js
@@ -1,21 +1,152 @@
 import React from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableHighlight,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar
+} from "react-native";
+import {
+  useFonts,
+  NovaSquare_400Regular
+} from "@expo-google-fonts/nova-square";
+import { AppLoading } from "expo";
+import * as Linking from "expo-linking";
+
+const links = [
+  {
+    id: 1,
+    name: "Google",
+    example: "heyhey whats shaking",
+    link: "https://google.com"
+  },
+  {
+    id: 2,
+    name: "Github",
+    example: "heyhey whats shaking",
+    link: "https://github.com"
+  },
+  {
+    id: 3,
+    name: "Mr. Bailey",
+    example: "heyhey whats shaking",
+    link:
+      "https://avatars3.githubusercontent.com/u/684976?s=460&u=99038daae23e0caf7a490ea8bd531bbedf90b06f&v=4"
+  },
+  {
+    id: 4,
+    name: "",
+    example: "",
+    link: ""
+  },
+  {
+    id: 5,
+    name: "",
+    example: "",
+    link: ""
+  }
+];
+
+const Item = ({ link, name, example }) => (
+  <TouchableHighlight
+    onPress={() => Linking.openURL(link)}
+    underlayColor="#39465A"
+    style={{ alignItems: "center", margin: 20 }}
+  >
+    <View style={styles.smallButton}>
+      <View style={styles.smallButtonInner}>
+        <Text style={styles.smallButtonTitleText}>{name}</Text>
+        <Text numberOfLines={2} style={styles.smallButtonText}>
+          {example}
+        </Text>
+      </View>
+    </View>
+  </TouchableHighlight>
+);
 
 const IdentifyNumbers = () => {
-  return (
-    <View style={styles.container}>
-      <Text>hi</Text>
-      <Text>heyhey whats shaking</Text>
-    </View>
+  const renderItem = ({ item }) => (
+    <Item link={item.link} name={item.name} example={item.example} />
   );
-};
 
+  const [fontsLoaded] = useFonts({
+    NovaSquare_400Regular
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.titleText}>Mixed Ciphers</Text>
+          <Text style={styles.subTitleText}></Text>
+        </View>
+        <FlatList
+          data={links}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
+    );
+  }
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#39465A"
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16
+  },
+  title: {
+    fontSize: 32
+  },
+  titleText: {
+    fontFamily: "NovaSquare_400Regular",
+    color: "#D7F8FF",
+    fontSize: 48,
+    textAlign: "center",
+    marginTop: 20,
+    marginBottom: 10
+  },
+  subTitleText: {
+    fontFamily: "NovaSquare_400Regular",
+    color: "#D7F8FF",
+    fontSize: 18,
+    textAlign: "center"
+  },
+  smallButton: {
+    width: 370,
+    aspectRatio: 10 / 3,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#CEF6FF",
+    padding: 12,
+    borderRadius: 30
   },
+  smallButtonInner: {
+    width: 355,
+    aspectRatio: 10 / 3 + 0.5,
+    backgroundColor: "white",
+    borderRadius: 25
+  },
+  smallButtonTitleText: {
+    textAlign: "center",
+    fontFamily: "NovaSquare_400Regular",
+    fontSize: 30
+  },
+  smallButtonText: {
+    textAlign: "left",
+    fontFamily: "NovaSquare_400Regular",
+    fontSize: 15,
+    padding: 10
+  }
 });
 
 export default IdentifyNumbers;
